0c8a2327bfdf7b2d230844ed780c6c9162a6d717
VictorZhou123 <victorzhoux@163.com>
2024-08-15 00:55:47 +0800

    feat: code adapt for `Test_subscribeImpl_Subscribe` and new an UT function `Test_subscribeImpl_Handle`

--- subscriber_test.go
+++ subscriber_test.go
@@ -1,39 +1,62 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
-var s1, s2 Subscriber
+const MsgBodyThisIsMessageBody = "this is message body"
 
-type sub1 struct{}
+var (
+	buf                                  = &bytes.Buffer{}
+	noTopicConsumer, withTopicConsumer   Consumer
+	noTopicConsumer2, withTopicConsumer2 Consumer
+	missTopicMsg, hitTopicMsg            Message
+)
 
-func (s *sub1) Consume(e Event) {
+type cs1 struct{}
+
+func (s *cs1) Consume(e Event) {
 	e.Message()
 }
 
-func (s *sub1) Topics() []string {
+func (s *cs1) Topics() []string {
 	return []string{"topic1", "topic2"}
 }
 
-type sub2 struct{}
+type cs2 struct{}
 
-func (s *sub2) Consume(e Event) {
+func (s *cs2) Consume(e Event) {
 	e.Message()
 }
 
-func (s *sub2) Topics() []string {
+func (s *cs2) Topics() []string {
 	return nil
 }
 
 func init() {
-	s1 = &sub1{}
-	s2 = &sub2{}
+	// Test_subscribeImpl_Subscribe
+	noTopicConsumer = &cs1{}
+	withTopicConsumer = &cs2{}
+
+	// Test_subscribeImpl_Handle
+	noTopicConsumer2 = &cs3{buf}
+	withTopicConsumer2 = &cs4{buf}
+	missTopicMsg = Message{
+		key:    "topic_not_hit",
+		Header: make(map[string]string),
+		Body:   []byte{},
+	}
+	hitTopicMsg = Message{
+		key:    "topic4",
+		Header: make(map[string]string),
+		Body:   []byte(MsgBodyThisIsMessageBody),
+	}
 }
 
 func Test_subscribeImpl_Subscribe(t *testing.T) {
 	type args struct {
-		sub Subscriber
+		sub Consumer
 	}
 	tests := []struct {
 		name string
@@ -42,13 +65,13 @@ func Test_subscribeImpl_Subscribe(t *testing.T) {
 		{
 			"no topics",
 			args{
-				s1,
+				noTopicConsumer,
 			},
 		},
 		{
 			"with topics return",
 			args{
-				s2,
+				withTopicConsumer,
 			},
 		},
 	}
@@ -56,6 +79,72 @@ func Test_subscribeImpl_Subscribe(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			s := NewSubscribeImpl()
 			s.Subscribe(tt.args.sub)
+		})
+	}
+}
+
+type cs3 struct {
+	buffer *bytes.Buffer
+}
+
+func (s *cs3) Consume(e Event) {
+	s.buffer.Write(e.Message().Body)
+}
+
+func (s *cs3) Topics() []string {
+	return nil
+}
+
+type cs4 struct {
+	buffer *bytes.Buffer
+}
+
+func (s *cs4) Consume(e Event) {
+	s.buffer.Write(e.Message().Body)
+}
+
+func (s *cs4) Topics() []string {
+	return []string{"topic3", "topic4"}
+}
+
+func Test_subscribeImpl_Handle(t *testing.T) {
+	type args struct {
+		c Consumer
+		e Event
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"miss topic",
+			args{
+				noTopicConsumer2,
+				missTopicMsg,
+			},
+			"",
+		},
+		{
+			"hit topic",
+			args{
+				withTopicConsumer2,
+				hitTopicMsg,
+			},
+			MsgBodyThisIsMessageBody,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSubscribeImpl()
+			s.Subscribe(tt.args.c)
+			s.Handle(tt.args.e)
+
+			got := buf.String()
+
+			if got != tt.want {
+				t.Errorf("got %s want %s", got, tt.want)
+			}
 		})
 	}
 }
